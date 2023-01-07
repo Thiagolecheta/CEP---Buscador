16a31fbd402161c0485d69a1cea1a7e5bf1ab5cb
Thiagolecheta <thiago.lecheta1@gmail.com>
2023-01-07 17:58:49 -0300

    Projeto que realiza busca do cep digitado e retorna dados em tela

---
+++ script/script.js
@@ -0,0 +1,30 @@
+
+    let answer = document.querySelector('.answer')
+
+async function searchCEP(){
+    let cep = document.querySelector(".inputCEP").value
+
+
+    const results = await fetch (`https://cdn.apicep.com/file/apicep/${cep}.json`)
+
+    let json = await results.json()
+
+
+    if(json.status === 200){
+
+        answer.style.display = 'block'
+        document.querySelector('.address').innerHTML = 'Endereço: ' + json.address;
+        document.querySelector('.district').innerHTML = 'Bairro: ' + json.district;
+        document.querySelector('.city').innerHTML = 'Cidade: ' + json.city;
+        document.querySelector('.state').innerHTML = 'Estado: ' + json.state;
+        document.querySelector('.code').innerHTML = 'Caixa Postal: ' + json.code;
+    } 
+
+    
+}
+
+
+function clear(){
+    answer.style.display = 'none'
+}
+
